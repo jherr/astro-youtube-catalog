47a4182fe2fcd1c418956f596822fd6066c96d7a
Jack Herrington <jherr@pobox.com>
2023-01-21 14:42:11 -0800

    Now with fuzzy search

--- src/pages/search.json.ts
+++ src/pages/search.json.ts
@@ -1,17 +1,42 @@
-import { getCollection } from "astro:content";
+import { CollectionEntry, getCollection } from "astro:content";
+import lunr from "lunr";
 import { URL } from "node:url";
 
-const videosPromise = getCollection("videos");
+type VideoId = Pick<CollectionEntry<"videos">, "id">["id"];
+
+const videosPromise = getCollection("videos").then((videos) => ({
+  videos,
+  index: lunr(function () {
+    this.ref("id");
+    this.field("title");
+    this.field("body");
+    this.field("tags");
+
+    videos.forEach((video) => {
+      this.add({
+        title: video.data.title,
+        body: video.body,
+        tags: video.data.tags,
+        id: video.id,
+      });
+    });
+  }),
+}));
 
 export async function get({ request }) {
-  const videos = await videosPromise;
+  const videoIndex = await videosPromise;
 
   const q = new URL(request.url).searchParams.get("q")?.toLowerCase();
+
+  const videos = videoIndex.index.search(q ?? "").map((res) => {
+    const id = res.ref as VideoId;
+    return videoIndex.videos.find((video) => video.id === id);
+  });
 
   return new Response(
     JSON.stringify(
       videos
-        .filter((video) => video.data.title.toLowerCase().includes(q ?? ""))
+        .filter((video) => video?.data.title.toLowerCase().includes(q ?? ""))
         .slice(0, 10)
     ),
     {
